904729c011d3b922b0d65a105149776ef37b4df7
KaziMuktadirAhmed <kazi.muktadir@brainstation-23.com>
2024-10-28 12:56:15 +0600

    restructure

--- web/frontend/pages/manual.jsx
+++ web/frontend/pages/manual.jsx
@@ -6,7 +6,11 @@ import {
   Text,
   Button,
   HorizontalStack,
+  AlphaCard,
+  Banner,
+  List,
 } from "@shopify/polaris";
+import { AlertDiamondIcon } from "@shopify/polaris-icons";
 import { Modal, useAppBridge } from "@shopify/app-bridge-react";
 import { useUninstall } from "../hooks/useUninstall";
 import { Redirect } from "@shopify/app-bridge/actions";
@@ -34,45 +38,80 @@ export default function manual() {
 
   return (
     <Page title="User manual">
-      <Layout>
-        {showConfirmation && (
-          <Modal
-            open={showConfirmation}
-            message="Are you sure you want to remove everything ?"
-            title="Remove everything"
-            secondaryActions={[
-              {
-                content: "No",
-                onAction: () => setShowConfirmation(false),
-              },
-            ]}
-            primaryAction={{
-              loading: isLoading,
-              content: "Yes",
-              destructive: true,
-              onAction: handleCancelSubscriptionClick,
-            }}
-            onClose={() => setShowConfirmation(false)}
-          ></Modal>
-        )}
-        <Layout.Section oneHalf>
-          <Text variant="headingLg">Reset everything</Text>
-          <Box paddingBlockStart={"1"} paddingBlockEnd={"2"}>
-            <Text variant="bodyMd">
-              This will remove every change that the app made in youre store,
-              including theme snippets and stored settings.
-            </Text>
-          </Box>
-          <Box></Box>
-        </Layout.Section>
-        <Layout.Section oneHalf>
-          <HorizontalStack align="center">
-            <Button destructive onClick={() => setShowConfirmation(true)}>
-              Reset
-            </Button>
-          </HorizontalStack>
-        </Layout.Section>
-      </Layout>
+      {showConfirmation && (
+        <Modal
+          open={showConfirmation}
+          message="Are you sure you want to remove everything ?"
+          title="Remove everything"
+          secondaryActions={[
+            {
+              content: "No",
+              onAction: () => setShowConfirmation(false),
+            },
+          ]}
+          primaryAction={{
+            loading: isLoading,
+            content: "Yes",
+            destructive: true,
+            onAction: handleCancelSubscriptionClick,
+          }}
+          onClose={() => setShowConfirmation(false)}
+        ></Modal>
+      )}
+      <Box paddingBlockEnd={"5"}>
+        <Layout>
+          <Layout.Section>
+            <AlphaCard padding={"0"}>
+              <Banner status="warning" title="App uninstall instructions" />
+              <Box padding={"4"}>
+                <Text variant="bodyMd">
+                  Our app enhances your store’s SEO by adding custom code to
+                  theme files, including meta tags, structured data, and image
+                  alt text optimizations. By clicking here, you’ll automatically
+                  remove all SEO code introduced by the app, restoring your
+                  theme to its original state and keeping your store’s code
+                  clean and streamlined after uninstalling.
+                </Text>
+                <Box paddingBlockStart={"6"}>
+                  <Text variant="headingMd">Instruction</Text>
+                  <Box paddingBlockStart={"2"}>
+                    <List>
+                      <List.Item>Click the reset Button</List.Item>
+                      <List.Item>Confirm reset everything</List.Item>
+                      <List.Item>Uninstall the app from app settings</List.Item>
+                      <List.Item>
+                        Do not go back to app home page after confirming reset.
+                        (This will reapply the theme edits to your store)
+                      </List.Item>
+                    </List>
+                  </Box>
+                </Box>
+              </Box>
+            </AlphaCard>
+          </Layout.Section>
+        </Layout>
+      </Box>
+      <Box>
+        <Layout>
+          <Layout.Section oneHalf>
+            <Text variant="headingLg">Reset everything</Text>
+            <Box paddingBlockStart={"1"} paddingBlockEnd={"2"}>
+              <Text variant="bodyMd">
+                This will remove every change that the app made in youre store,
+                including theme snippets and stored settings.
+              </Text>
+            </Box>
+            <Box></Box>
+          </Layout.Section>
+          <Layout.Section oneHalf>
+            <HorizontalStack align="center">
+              <Button destructive onClick={() => setShowConfirmation(true)}>
+                Reset
+              </Button>
+            </HorizontalStack>
+          </Layout.Section>
+        </Layout>
+      </Box>
     </Page>
   );
 }
