9c27b8f8f759e10722612d685e6f6ccb2455fad9
shafi10 <shafiswe@gmail.com>
2024-11-04 15:24:35 +0600

    openai setup

--- web/config/config.js
+++ web/config/config.js
@@ -1,6 +1,4 @@
 // Set your Azure OpenAI API key and endpoint
-const apiKey = process.env.AI_API_KEY;
-const endpoint = process.env.AI_API_ENDPOINT;
-const deploymentName = process.env.AI_DEPLOYMENT_NAME;
-
-console.log(AI_API_ENDPOINT, "this is a deployment");
+export const apiKey = process.env.AI_API_KEY;
+export const endPoint = process.env.AI_API_ENDPOINT;
+export const deploymentName = process.env.AI_DEPLOYMENT_NAME;
---
+++ web/utils/getAIContext.js
@@ -0,0 +1,33 @@
+import { Configuration, OpenAIApi } from "azure-openai";
+import { apiKey, endPoint, deploymentName } from "../config/config";
+
+class AzureOpenAIService {
+  constructor() {
+    this.openAiApi = new OpenAIApi(
+      new Configuration({
+        apiKey: apiKey,
+        azure: {
+          apiKey: apiKey,
+          endpoint: endPoint,
+          deploymentName: deploymentName,
+        },
+      })
+    );
+  }
+
+  async getAIResults(messages) {
+    const response = await this.openAiApi.createChatCompletion({
+      model: deploymentName,
+      messages: messages, //[{ role: "user", content: "hi and hello" }],
+      max_tokens: 500, // Corrected parameter
+      temperature: 0.9,
+      top_p: 1, // Corrected to `top_p` as well
+      presence_penalty: 0,
+      frequency_penalty: 0,
+    });
+
+    return response;
+  }
+}
+
+export default AzureOpenAIService;
