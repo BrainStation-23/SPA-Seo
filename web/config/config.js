9924541f915a6227ee98647bae4f0460ff6f01e3
shafi10 <shafiswe@gmail.com>
2024-11-07 18:28:07 +0600

    remove config

--- web/config/config.js
+++ web/config/config.js
@@ -1,4 +1,4 @@
 // Set your Azure OpenAI API key and endpoint
 export const apiKey = process.env.AI_API_KEY;
-process.env.AI_API_ENDPOINT;
+export const endPoint = process.env.AI_API_ENDPOINT;
 export const deploymentName = process.env.AI_DEPLOYMENT_NAME;
